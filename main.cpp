a98809260e693e0847104d431285935064fdbcfb
Pulkit Garg <pulkitgarg784@gmail.com>
2023-05-17 23:30:22 -0400

    Output basic color gradiant.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(){
+
+    // Set initial consts
+    const int img_width = 256;
+    const int img_height = 256;
+
+    // Render the output
+    std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
+    for (int j = img_height -1; j >= 0 ; --j) {
+        for (int i = 0; i < img_width; ++i) {
+            auto r = double(i) / (img_width - 1);
+            auto g = double(j) / (img_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
