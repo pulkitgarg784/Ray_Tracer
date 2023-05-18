5afc39d26a0701eaa1747958d5fb08b20d08e1a6
Pulkit Garg <pulkitgarg784@gmail.com>
2023-05-17 23:39:46 -0400

    Added Progress Indicator

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(){
     // Render the output
     std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
     for (int j = img_height -1; j >= 0 ; --j) {
+        std::cerr << "\rProgress: " << (img_height - j) * 100 /img_height << '%' << std::flush;
         for (int i = 0; i < img_width; ++i) {
             auto r = double(i) / (img_width - 1);
             auto g = double(j) / (img_height - 1);
@@ -21,4 +22,5 @@ int main(){
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "\nDone.\n";
 }
